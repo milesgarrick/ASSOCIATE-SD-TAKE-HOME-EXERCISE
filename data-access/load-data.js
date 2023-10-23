993d771a0b602b93459bc412078076a216e7ef82
Miles Johnson <mgj2@pdx.edu>
2023-10-23 16:18:51 -0700

    Wrote function to transform Pet Project data

--- data-access/load-data.js
+++ data-access/load-data.js
@@ -13,6 +13,41 @@ export function loadData() {
 }
 
 /**
+ * Sorts by Dog then Cat, then sorts by kennel size if same type.
+ *
+ * @param {Animal} a
+ * @param {Animal} b
+ * @returns {Animal}
+ */
+function sortFunction(a, b) {
+  console.log(a.type, b.type);
+  if (a.type === b.type) {
+    return compareKennelSize(a, b);
+  } else if (a.type === 'Dog') {
+    console.log(a.name + '>' + b.name);
+    return -1;
+  } else {
+    console.log(b.name + '>' + a.name);
+    return 1;
+  }
+}
+
+/**
+ * Sorts from small to large.
+ *
+ * @param {Animal} a
+ * @param {Animal} b
+ * @returns {Animal}
+ */
+function compareKennelSize(a, b) {
+  if (a.kennelSize === 'Small') return -1;
+  else if (b.kennelSize === 'Small') return 1;
+  else if (a.kennelSize === 'Medium') return -1;
+  else if (b.kennelSize === 'Medium') return 1;
+  else return -1;
+}
+
+/**
  * @TODO Write this function
  *
  * Merges data from both sources and sorts it first by animal type (cat/dog) and
@@ -24,17 +59,98 @@ export function loadData() {
  * @param {PetSanctuary} petSanctuary
  * @returns {Animal[]}
  */
+
+/**
+ * Maps kennel IDs to sizes, then iterates through the array, storing values and
+ * applying title case.
+ *
+ * @param {PetProject} petProject
+ * @returns {Animal[]}
+ */
+function transformPetProjectData(petProject) {
+  const animals = [];
+  const kennelMap = new Map([
+    [1, 'small'],
+    [2, 'small'],
+    [3, 'small'],
+    [4, 'medium'],
+    [5, 'medium'],
+    [6, 'large'],
+  ]);
+  petProject.animals.forEach((e) => {
+    var newType = e.type.charAt(0).toUpperCase() + e.type.slice(1);
+    var newName = e.name.charAt(0).toUpperCase() + e.name.slice(1);
+    var newBreed = e.breed.charAt(0).toUpperCase() + e.breed.slice(1);
+    var newKennelSize =
+      kennelMap.get(e.kennelId).charAt(0).toUpperCase() +
+      kennelMap.get(e.kennelId).slice(1);
+    var animal = {
+      type: newType,
+      name: newName,
+      breed: newBreed,
+      kennelSize: newKennelSize,
+    };
+    animals.push(animal);
+  });
+  return animals;
+}
+
+function transformPetSanctuaryData(petSanctuary) {
+  const animals = [];
+  let kennels = Object.entries(petSanctuary.shelter);
+  kennels.forEach((kennel) => {
+    var newType;
+    var newName;
+    var newBreed;
+    var newKennelSize = kennel[1].size;
+    if (kennel[1].animal) {
+      newType = kennel[1].animal.species;
+      newName = kennel[1].animal.name;
+      if (kennel[1].animal.breed) {
+        newBreed = kennel[1].animal.breed;
+      } else {
+        newBreed = 'Unknown';
+      }
+    } else {
+      //Accounts for potential typo in the animal key
+      newType = kennel[1].animals.species;
+      newName = kennel[1].animals.name;
+      if (kennel[1].animals.breed) {
+        newBreed = kennel[1].animals.breed;
+      } else {
+        newBreed = 'Unknown';
+      }
+    }
+
+    newType = newType.charAt(0).toUpperCase() + newType.slice(1);
+    newName = newName.charAt(0).toUpperCase() + newName.slice(1);
+    newBreed = newBreed.charAt(0).toUpperCase() + newBreed.slice(1);
+    newKennelSize =
+      newKennelSize.charAt(0).toUpperCase() + newKennelSize.slice(1);
+    var animal = {
+      type: newType,
+      name: newName,
+      breed: newBreed,
+      kennelSize: newKennelSize,
+    };
+
+    animals.push(animal);
+  });
+  return animals;
+}
+
 function mergeAndSortData(petProject, petSanctuary) {
-  return {
-    animals: [
-      {
-        type: 'Unknown',
-        name: 'Unknown',
-        breed: 'Unknown',
-        kennelSize: 'Unknown',
-      },
-    ],
-  };
+  const petProjectTransformed = transformPetProjectData(petProject);
+  const petSanctuaryTransformed = transformPetSanctuaryData(petSanctuary);
+
+  const allAnimals = petProjectTransformed
+    .concat(petSanctuaryTransformed)
+    .sort(sortFunction);
+  console.log(allAnimals);
+
+  return petProjectTransformed
+    .concat(petSanctuaryTransformed)
+    .sort(sortFunction);
 }
 
 /**
